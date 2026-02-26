f160300487c8de0e563c9b3f14e7f8b7b0c1fe59
Tuan Nguyen <anhtuan959021@gmail.com>
2026-02-26 15:17:15 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+// ===== MOBILE MENU TOGGLE =====
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const overlay = document.querySelector(".overlay-menu");
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+
+    document.body.style.overflow =
+        navLinks.classList.contains("active") ? "hidden" : "auto";
+});
+
+overlay.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    navLinks.classList.remove("active");
+    overlay.classList.remove("active");
+    document.body.style.overflow = "auto";
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        menuToggle.classList.remove("active");
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+        document.body.style.overflow = "auto";
+    });
+});
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        navLinks.classList.remove("active");
+        overlay.classList.remove("active");
+        menuToggle.classList.remove("active");
+        document.body.style.overflow = "auto";
+    }
+});
+// ===== SMOOTH SCROLL =====
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 70,
+                behavior: "smooth"
+            });
+        }
+    });
+});
+
+// ===== Header Change On Scroll =====
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+
+
+
+
+// ===== SCROLL FADE-IN EFFECT =====
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+sections.forEach(section => {
+    section.style.opacity = "0";
+    section.style.transform = "translateY(40px)";
+    section.style.transition = "all 0.6s ease";
+    observer.observe(section);
+});
+
